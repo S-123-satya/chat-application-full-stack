a27fadad716bf98c992a5589af252ae612f6f1a1
S-123-satya <satyaprakash5056742@gmail.com>
2023-08-10 17:59:18 +0530

    jwt added

--- controllers/postLoginController.js
+++ controllers/postLoginController.js
@@ -1,17 +1,22 @@
 const User = require("../models/userModel");
 const checkPassword = require("../utils/checkPassword");
 
-module.exports.postLoginController=async (req,res)=>{
-    const result=await User.findOne({
-        where:{
-            email:req.body.email,
+module.exports.postLoginController = async (req, res) => {
+    const result = await User.findOne({
+        where: {
+            email: req.body.email,
         }
     })
-    if(result){
-        const varify=await checkPassword(req.body.password,result.password);
-        res.json({message:'login'});
+    if (result) {
+        const varify = await checkPassword(req.body.password, result.password);
+        if (varify){
+            const token=generateToken(result.id,result.email)
+            res.json({message:"User login successfully",status:201});
+        }
+        else
+            res.json({ message: "Incorrect password", status: 401 })
     }
-    else{
-        res.json({message:'User not found'});
+    else {
+        res.json({ message: 'User not found', status: 404 });
     }
 }
