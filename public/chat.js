f4092f0ea1218d36ef607cbed2cc07983e3928cf
S-123-satya <satyaprakash5056742@gmail.com>
2023-08-14 02:10:27 +0530

    DomContentLoaded frontend completed

--- public/chat.js
+++ public/chat.js
@@ -1,39 +1,45 @@
-const url='http://localhost:3000'
-const config={
+const url = 'http://localhost:3000'
+const config = {
     headers: {
         Authorization: localStorage.getItem('token'),
     }
 }
 setInterval(async function () {
-    const data=await axios.get(`${url}/message/chats`,config);
+    const data = await axios.get(`${url}/message/chats`, config);
     const userList = document.getElementById('userList');
-    userList.innerHTML=""
+    userList.innerHTML = ""
     console.log(data?.data?.data);
-    (data?.data?.data).map((ele)=>{
-        displayMessage(ele?.User?.name,ele?.message,ele?.id);
+    (data?.data?.data).map((ele) => {
+        displayMessage(ele?.User?.name, ele?.message, ele?.id);
     })
 }, 1000);
-const displayMessage=(name,message,id)=>{
+const displayMessage = (name, message, id) => {
     const userList = document.getElementById('userList');
-    let element=document.createElement('tr')
-    element.id=id;
-    element.innerHTML=`<td>${name}: ${message}</td>`
+    let element = document.createElement('tr')
+    element.id = id;
+    element.innerHTML = `<td>${name}: ${message}</td>`
     userList.appendChild(element);
-    
+
 }
 const sendMessage = document.getElementById('sendMessage');
-sendMessage.addEventListener('click',async(e)=>{
+sendMessage.addEventListener('click', async (e) => {
     e.preventDefault();
     const inputChat = document.getElementById('inputChat');
-    const message={
-        message:inputChat?.value,
+    const message = {
+        message: inputChat?.value,
     }
-    const data=await axios.post(`${url}/message`,message,config);
-    displayMessage("You",data?.data?.data?.message,data?.data?.data?.id);
-    inputChat.value="";
+    const data = await axios.post(`${url}/message`, message, config);
+    displayMessage("You", data?.data?.data?.message, data?.data?.data?.id);
+    inputChat.value = "";
     inputChat.focus()
 })
-
+window.addEventListener('DOMContentLoaded', async() => {
+    const message = localStorage.getItem('messages');
+    console.log(message);
+    const id = message ? message[message?.length - 1]?.id : 1;
+    console.log(id);
+    const data=await axios.get(`${url}/message/chats?id=${id}`)
+})
 // const themeselector=document.getElementById('themeselector');
 // const theme =localStorage.getItem('theme');
 // changeTheme(theme);
@@ -47,7 +53,7 @@ sendMessage.addEventListener('click',async(e)=>{
 //         document.body.style.backgroundColor='#222';
 //     else if(theme==='light')
 //     document.body.style.backgroundColor='#e5e5e5';
-//     else 
+//     else
 //     document.body.style.backgroundColor='#fff';
 // }
 // window.addEventListener('storage',(e)=>{
