fc25c0ac18c340d013062c51d9d9c926a47d35a6
S-123-satya <satyaprakash5056742@gmail.com>
2023-08-21 11:36:27 +0530

    group list fetch and display to the frontend

--- public/chat.js
+++ public/chat.js
@@ -103,13 +103,14 @@ async function getGroupList() {
     if (list === null || list === undefined) {
         const data = await axios.get(`${url}/group`, config);
         console.log(data);
-        localStorage.setItem('groupList', data?.data?.data)
-        data?.data?.data.map(list => displayGroup(list));
+        const stringObj=JSON.stringify(data?.data?.groupList[0]?.groups);
+        localStorage.setItem('groupList', stringObj)
+        data?.data?.groupList[0]?.groups?.map(list => displayGroup(list));
     }
     else {
         list = JSON.parse(list)
         console.log(list);
-        displayGroup(list)
+        list.map(ele=>displayGroup(ele));
     }
 }
 function displayGroup(list) {
@@ -118,17 +119,16 @@ function displayGroup(list) {
     console.log(groupList);
     const div = document.createElement('div');
     div.className = `side__lower-contact`;
-    const insertBelowThis = document.getElementById('insertBelowThis');
+    div.id=list?.id;
     div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
     <path
       d="M206.7 378.1c10.23 7.832 24.32 7.832 34.55 0C282.6 346.3 384 259.6 384 173.6C384 65.83 312.3 0 224 0C135.6 0 64 65.83 64 173.6C64 259.6 165.4 346.3 206.7 378.1zM240 236c0-33.13 26.88-60 60-60h24c6.625 0 12 5.375 12 12v8C336 229.1 309.1 256 276 256h-24C245.4 256 240 250.6 240 244V236zM112 196v-8C112 181.4 117.4 176 124 176h24C181.1 176 208 202.9 208 236v8C208 250.6 202.6 256 196 256h-24C138.9 256 112 229.1 112 196zM319.7 352.5c-20.72 20.3-41.57 37.69-58.92 51.03C250.2 411.6 237.2 416 224 416s-26.23-4.43-36.78-12.51c-17.33-13.33-38.17-30.7-58.88-50.99C57.07 355.2 0 413.4 0 485.3C0 500.1 11.94 512 26.66 512H421.3C436.1 512 448 500.1 448 485.3C448 413.4 390.9 355.2 319.7 352.5z" />
   </svg>
   <div class="side__lower-contact-textbox">
-    <div class="side__lower-contact-name">${list?.data?.data?.groupname}</div>
+    <div class="side__lower-contact-name">${list?.groupname}</div>
     <div class="side__lower-contact-text">Lorem ipsum dolor sit amet...</div>
   </div>`
-
-    insertAfter(div, insertBelowThis)
+    groupList.appendChild(div);
 }
 
 function insertAfter(newNode, existingNode) {
