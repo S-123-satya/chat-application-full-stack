9834f391114114fff744bbf00ecf1680be7a6c07
S-123-satya <satyaprakash5056742@gmail.com>
2023-08-31 09:11:42 +0530

    Admin is verify every time when there is a task of admin(remove admin,add admin etc)

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { postGroupController, getGroupController, getGroupUserController, removeGroupAdminController, addGroupAdminController, addGroupUserController, removeGroupUserController } = require("../controllers/groupController");
-const { verifyToken, verifyUser } = require("../utils/tokensUtils");
+const { verifyToken, verifyUser, checkAdmin } = require("../utils/tokensUtils");
 
 const router = Router();
 
@@ -8,8 +8,8 @@ router.post('/', verifyToken, verifyUser, postGroupController);
 router.get('/', verifyToken, verifyUser, getGroupController);
 router.get('/user/:id', verifyToken, verifyUser, getGroupUserController);
 // verify admin in all controllers => make a middleware to verify that is it group admin or not
-router.get('/admin/addadmin', verifyToken, verifyUser, addGroupAdminController);
-router.delete('/admin/removeadmin', verifyToken, verifyUser, removeGroupAdminController);
-router.get('/admin/adduser', verifyToken, verifyUser, addGroupUserController);
-router.delete('/admin/removeuser', verifyToken, verifyUser, removeGroupUserController);
+router.get('/admin/addadmin', verifyToken, verifyUser, checkAdmin, addGroupAdminController);
+router.delete('/admin/removeadmin', verifyToken, verifyUser, checkAdmin, removeGroupAdminController);
+router.get('/admin/adduser', verifyToken, verifyUser, checkAdmin, addGroupUserController);
+router.delete('/admin/removeuser', verifyToken, verifyUser, checkAdmin, removeGroupUserController);
 module.exports = router;
--- utils/tokensUtils.js
+++ utils/tokensUtils.js
@@ -1,24 +1,42 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const Group = require("../models/groupModel");
 
-module.exports.generateToken=(id,email)=>{
+module.exports.generateToken = (id, email) => {
     console.log(id);
     console.log(email);
-    return jwt.sign({id:id,email:email},process.env.JWT_SECRET_KEY)
+    return jwt.sign({ id: id, email: email }, process.env.JWT_SECRET_KEY)
 }
-module.exports.verifyToken=(req,res,next)=>{
-    const token=req.headers?.authorization;
-    req.body.tokenObj=jwt.verify(token,process.env.JWT_SECRET_KEY)
+module.exports.verifyToken = (req, res, next) => {
+    const token = req.headers?.authorization;
+    req.body.tokenObj = jwt.verify(token, process.env.JWT_SECRET_KEY)
     next();
 }
 
-module.exports.verifyUser=async(req,res,next)=>{
-    const {tokenObj}=req.body;
-    const result=await User.findOne({where:{
-        id:tokenObj.id
-    }})
-    if(result)
+module.exports.verifyUser = async (req, res, next) => {
+    const { tokenObj } = req.body;
+    const result = await User.findOne({
+        where: {
+            id: tokenObj.id
+        }
+    })
+    if (result)
         next();
     else
-        res.json({message:"User does not exist",status:401})
+        res.json({ message: "User does not exist", status: 401 })
+}
+
+module.exports.checkAdmin = async (req, res, next) => {
+    const { tokenObj } = req.body;
+    const { groupId } = req.query;
+    const groupData = await Group.findByPk(groupId);
+    console.log(groupData);
+    if (groupData) {
+        const check=await groupData.hasAdmin({where:{id:tokenObj.id}});
+        if(check)
+            next();
+        else
+            res.json({message:'You are not an Admin'});
+    }
+    else res.json({ message: 'something went wrong' });
 }
